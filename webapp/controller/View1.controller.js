79f520d365991a64a211269dfdd743b775e79bb5
ramana24 <yellapuramana@gmail.com>
2024-05-11 06:10:50 +0000

    Revised KPi v1.3

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -24,6 +24,45 @@ sap.ui.define([
                   var oSAPModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001997')?$format=json");
             
                   this.getView().byId("IDcontainer").setModel(oSAPModel,"oSAPModel");
+
+
+                   // card 2 model
+                   var oConModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001998')?$format=json");
+            
+                   this.getView().byId("Concontainer").setModel(oConModel,"oConModel");
+
+                      // card 3 model
+                      var oAriModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001999')?$format=json");
+            
+                      this.getView().byId("Aribacontainer").setModel(oAriModel,"oAriModel");
+
+                          // card 4 model
+                          var oFGModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001994')?$format=json");
+            
+                          this.getView().byId("FGcontainer").setModel(oFGModel,"oFGModel");
+
+                             // card 5 model
+                             var oECCModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001995')?$format=json");
+            
+                             this.getView().byId("ECCcontainer").setModel(oECCModel,"oECCModel");
+
+                                   // card 6 model
+                                   var oJavaModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001996')?$format=json");
+            
+                                   this.getView().byId("Javcontainer").setModel(oJavaModel,"oJavaModel");
+
+
+                                      // card 7 model
+                                      var oSFModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001989')?$format=json");
+            
+                                      this.getView().byId("SFcontainer").setModel(oSFModel,"oSFModel");
+
+                                     // card 8 model
+                                     var oAZModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001992')?$format=json");
+            
+                                     this.getView().byId("Azcontainer").setModel(oAZModel,"oAZModel");
+
+
             }
         });
     });
