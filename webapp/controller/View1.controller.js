b833b613eddb0899510d5d4c1399d6ec183f8a59
ramana24 <yellapuramana@gmail.com>
2024-05-14 03:41:40 +0000

    Viz Charts v2.0

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "ux/zanalytics/util/Formatter",
+    "sap/ui/model/json/JSONModel",
     
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,Formatter) {
+    function (Controller,Formatter,JSONModel) {
         "use strict";
 
         return Controller.extend("ux.zanalytics.controller.View1", {
@@ -47,6 +48,10 @@ sap.ui.define([
                              this.getView().byId("ECCcontainer").setModel(oECCModel,"oECCModel");
 
                                    // card 6 model
+                                   var oPOId="300001996";
+
+                              //     var oJavaModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('+oPOId+')?$format=json");
+            
                                    var oJavaModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001996')?$format=json");
             
                                    this.getView().byId("Javcontainer").setModel(oJavaModel,"oJavaModel");
@@ -61,6 +66,183 @@ sap.ui.define([
                                      var oAZModel =  new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders('300001992')?$format=json");
             
                                      this.getView().byId("Azcontainer").setModel(oAZModel,"oAZModel");
+
+                                     // KPI 1 
+
+                                    
+
+                                     var sUrl ="/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrderItems(POId='300001998',POItemPos='30')?$select=Price,GrossAmount&$format=json";
+
+                                     var CompModel = new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrderItems(POId='300001998',POItemPos='30')?$select=Price,GrossAmount&$format=json");
+            
+                                     this.getView().byId("IDKPIComp").setModel(CompModel,"CompModel");
+
+
+                                     // KPI 2
+
+                                     var oLineModel = new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PO_APV_SRV/PurchaseOrders?$select=ItemCount&$format=json");
+            
+                                     this.getView().byId("IDLineChart").setModel(oLineModel,"oLineModel");
+
+
+// Chart Model
+
+var oVizFrame = this.getView().byId("idVizFrameBar");
+
+
+       var aData = {
+        Items : [  
+            {
+                Flavor:"Blue Moon",
+                Sales : 700
+            },
+            {
+                Flavor:"Matcha Green Tea",
+                Sales : 1100
+            },
+            {
+                Flavor:"ButterScotch",
+                Sales : 1400
+            },
+            {
+                Flavor:"Black Current",
+                Sales : 560
+            }
+            ]
+};
+var oIceCreamModel = new sap.ui.model.json.JSONModel();
+oIceCreamModel.setData(aData);
+this.getView().byId("chartContainer").setModel(oIceCreamModel, "IceCreamModel");
+
+
+// PIE Model
+
+//var oVizFrame = this.getView().byId("idVizFramePie");
+
+
+       var aData = {
+        Items : [  
+            {
+                Flavor:"Blue Moon",
+                Sales : 700
+            },
+            {
+                Flavor:"Matcha Green Tea",
+                Sales : 1100
+            },
+            {
+                Flavor:"ButterScotch",
+                Sales : 1400
+            },
+            {
+                Flavor:"Black Current",
+                Sales : 560
+            }
+            ]
+};
+var oIceCreamModel = new sap.ui.model.json.JSONModel();
+oIceCreamModel.setData(aData);
+this.getView().byId("chartContainer2").setModel(oIceCreamModel, "IceCreamModel");
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Refernce.
+
+// 	 /*Total -Summary*/
+// 	 var oVizFrame = this.getView().byId("idVizFrameColumn");
+// 	 var oModel = new JSONModel();
+   
+   
+//      var data = {
+// 		   'Count' : [
+// 			   {"Status": "Total","Value": "1234","Value2":"456"},
+// 			   {"Status": "Inbound","Value": "9127","Value2":"569"},
+			  
+// 				{"Status": "Outbound","Value": "15957","Value2":"700"},
+// 				{"Status": "Profit","Value": "20000","Value2":"23000"},
+				
+// 			  ]};
+//    oModel.setData(data);
+   
+//    var oDataset = new sap.viz.ui5.data.FlattenedDataset({
+// 	   dimensions : [{
+// 		   name : 'Status',
+// 		   value : "{Status}"}],
+					  
+// 	   measures : [
+//            {
+// 		   name : 'Count',
+//            value : '{Value}'
+//            },
+//            {
+// 		   name : 'Count2',
+//            value : '{Value2}'
+//            }
+//                   ],
+        
+					
+// 	   data : {
+// 		   path : "/Count"
+// 	   }
+//    });		
+//    oVizFrame.setDataset(oDataset);
+//    oVizFrame.setModel(oModel);	
+//    oVizFrame.setVizType('bar');
+
+// // below all charts are supported
+// // Chart customizations property, aim to customize existing (build-in) charts to meet specific LoB requirements. Currently, supported chart type : column, dual_column, bar, dual_bar, stacked_column, stacked_bar, 100_stacked_bar, 100_stacked_column, 100_dual_stacked_bar, 100_dual_stacked_column, dual_stacked_bar, dual_stacked_column, line, horizontal_line, dual_line, dual_horizontal_line, combination, horizontal_combination, stacked_combination, horizontal_stacked_combination, dual_stacked_combination, dual_horizontal_stacked_combination, scatter, bubble.   
+// // 
+   
+   
+//    oVizFrame.setVizProperties({
+   
+// 	   plotArea: {
+// 	  // 	colorPalette : d3.scale.category20().range
+// 	   dataLabel: {
+// 					   visible: "true"
+// 				   }
+				
+// 		   }});
+   
+//    var feedValueAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+// 		 'uid': "valueAxis",
+// 		 'type': "Measure",
+// 		 'values': ["Count"],
+// 		 'label':"true"
+// 	   }), 
+// 	   feedDataAxis =   new sap.viz.ui5.controls.common.feeds.FeedItem({
+// 		'uid': "DataAxis",
+// 		'type': "Measure",
+// 		'values': ["Count"],
+// 		'label':"true"
+// 	  }), 
+// 	   feedCategoryAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+// 		 'uid': "categoryAxis",
+// 		 'type': "Dimension",
+// 		 'values': ["Status"]
+// 	   });
+//    oVizFrame.addFeed(feedValueAxis);
+//    oVizFrame.addFeed(feedCategoryAxis);
+//    oVizFrame.addFeed(feedDataAxis);
+
+
+
+
+
 
 
             }
